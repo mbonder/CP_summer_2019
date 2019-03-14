761d13baec690822ade17fc440d2b5bf44a4bc86
mbonder <mb73281@student.sgh.waw.pl>
2019-03-14 13:07:56 +0100

    first class

--- src/Loops.java
+++ src/Loops.java
@@ -2,5 +2,15 @@ public class Loops {
 
     public static void main(String[] args) {
 
+        Integer a = 6;
+
+        for (int i=0; i<5;i++) {
+            System.out.println("i=" + i);
+        }
+        for (int j=10;j>0;j--) {
+            if (j==8) continue; //skipping the 8
+            System.out.println("j=" +j);
+            if (j==6) break; //stopping the loop on number 6
+        }
     }
 }
