1c4a554e7205d05fad4ed01e7165afc39a35cb38
maksb <maksbonder@gmail.com>
2019-03-28 10:31:03 +0100

    3 ćw

---
+++ src/SumArgs.java
@@ -0,0 +1,12 @@
+public class SumArgs {
+
+
+    public static void main(String[] args) {
+        double sum = 0;
+        for (int i=0; i<args.length;i++) {
+            String arg = args[i];
+            sum += Double.valueOf(arg);
+        }
+        System.out.println("Sum: "+ sum);   //debug in home
+    }
+}
