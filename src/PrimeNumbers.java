a43ad503718595b629e801cf3d5b85b7dc329cb4
maksb <maksbonder@gmail.com>
2019-03-28 10:19:11 +0100

    3 ćw

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,13 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int n = 20;
+        for (int i = 2; i < n; i++) {
+             if ((n % i) == 0)
+             System.out.println(n + " is not a prime number");
+        }
+
+    }
+}
+
+
+// dokończyć w domu
